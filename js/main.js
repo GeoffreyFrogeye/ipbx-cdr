b3ef66f5f656d1e77613e2607672ed043dfec4a3
Geoffrey Preud'homme <geoffrey.preudhomme@ipsolution.fr>
2015-06-29 14:54:37 +0200

    Better filtering using $.data()
    Allows multiple filters per DOM element

--- js/main.js
+++ js/main.js
@@ -57,6 +57,75 @@ Array.prototype.diff = function(a) {
     });
 }; // From http://stackoverflow.com/a/4026828
 
+(function() {
+
+    var matcher = /\s*(?:((?:(?:\\\.|[^.,])+\.?)+)\s*([!~><=]=|[><])\s*("|')?((?:\\\3|.)*?)\3|(.+?))\s*(?:,|$)/g;
+
+    function resolve(element, data) {
+
+        data = data.match(/(?:\\\.|[^.])+(?=\.|$)/g);
+
+        var cur = jQuery.data(element)[data.shift()];
+
+        while (cur && data[0]) {
+            cur = cur[data.shift()];
+        }
+
+        return cur || undefined;
+
+    }
+
+    jQuery.expr[':'].data = function(el, i, match) {
+
+        matcher.lastIndex = 0;
+
+        var expr = match[3],
+            m,
+            check, val,
+            allMatch = null,
+            foundMatch = false;
+
+        while (m = matcher.exec(expr)) {
+
+            check = m[4];
+            val = resolve(el, m[1] || m[5]);
+
+            switch (m[2]) {
+                case '==':
+                    foundMatch = val == check;
+                    break;
+                case '!=':
+                    foundMatch = val != check;
+                    break;
+                case '<=':
+                    foundMatch = val <= check;
+                    break;
+                case '>=':
+                    foundMatch = val >= check;
+                    break;
+                case '~=':
+                    foundMatch = RegExp(check).test(val);
+                    break;
+                case '>':
+                    foundMatch = val > check;
+                    break;
+                case '<':
+                    foundMatch = val < check;
+                    break;
+                default:
+                    if (m[5]) foundMatch = !!val;
+            }
+
+            allMatch = allMatch === null ? foundMatch : allMatch && foundMatch;
+
+        }
+
+        return allMatch;
+
+    };
+
+}()); // From http://stackoverflow.com/a/2895933
+
 Array.prototype.transposeObjects = function() {
     var that = this;
     if (this.length) {
@@ -71,6 +140,10 @@ Array.prototype.transposeObjects = function() {
         return {};
     }
 }; // From http://stackoverflow.com/a/17428705
+
+// STATS FUNCTIONS
+var SS_X_FUN = ['mean', 'sum', 'mode', 'variance', 'standard_deviation', 'standard_deviation', 'median', 'geometric_mean', 'harmonic_mean', 'root_mean_square', 'min', 'max', 'sample_variance'];
+var C3_N_FUN = ['bar', 'pie', 'donut'];
 
 function queryDB(cb) {
     var status = $('.db .status');
@@ -88,192 +161,196 @@ function queryDB(cb) {
         });
 }
 
-function fillStat(el) {
-    el = $(el);
-    async.waterfall([
-        function filterCalls(cb) { // OPTZ Filter at filter elements, not stat elements
-            var calls = cdr.data;
+function updateFilter(fil, cb) { // Update a single filter
+    // Defining vars
+    fil = $(fil);
+    var parentData = fil.parents(':data(calls)').first();
+    var calls = parentData.length ? parentData.data('calls') : cdr.data;
+    var filterDef = fil.data('filters');
+    if (!filterDef) filterDef = [];
 
-            function filter(filterElement, cbf) {
-                function applyFilter(func) {
-                    async.filter(
-                        calls,
-                        func,
-                        function(data) {
-                            calls = data;
-                            cbf();
-                        }
-                    );
-                }
-
-                var filterName = filterElement.attr('data-filter-field'),
-                    filterPattern = filterElement.attr('data-filter-pattern');
-
-                if (filterName) {
-                    if (!calls[0][filterName]) cb("No such field for filter " + filterName);
-                    if (!filterPattern) cb("No pattern field for filter " + filterName);
-
-                    var flags = filterElement.attr('data-filter-flags');
-                    if (flags !== undefined) {
-                        var regexp = new RegExp(filterPattern, flags);
-                        applyFilter(function fieldRegExpFilter(call, cba) {
-                            cba(call[filterName].match(regexp));
-                        });
-                    } else {
-                        applyFilter(function fieldWholeFilter(call, cba) {
-                            cba(call[filterName] == filterPattern);
-                        });
-                    }
-                } else {
-                    var filterType = filterElement.attr('data-filter');
-                    switch (filterType) {
-                        case 'first':
-                            cb(null, [calls[0]]);
-                            break;
-
-                        case 'last':
-                            cb(null, [calls[calls.length-1]]);
-                            break;
-
-                        // TODO ≥/≤ date
-
-                        default:
-                            cb("Unknown filter type " + filterType);
-                    }
-                }
-            }
-
-            async.parallel([
-                function(cba) {
-                    if (el.attr('data-filter') || el.attr('data-filter-field')) {
-                        filter(el, cba);
-                    } else {
-                        cba();
-                    }
-                },
-                function(cba) {
-                    async.each(el.parents('[data-filter],[data-filter-field]'), function(data, cbe) {
-                        filter($(data), cbe);
-                    }, function(err) {
-                        cba(err);
-                    });
-                }
-
-            ], function(err) {
-                cb(err, calls);
-            });
-
-        },
-        function transposeData(filteredCalls, cb) {
-            cb(null, filteredCalls.transposeObjects()[el.attr('data-field')]);
-        },
-        function generateStat(x, cb) {
-            if (x && x.length) {
-                var stat = el.attr('data-stat'),
-                    numbers = typeof x[0] == 'number'; // Are numbers
-
-                var occurs = null;
-                if (!numbers) {
-                    occurs = x.reduce(function(occ, cur) {
-                        if (isNaN(occ[cur])) {
-                            occ[cur] = 1;
-                        } else {
-                            occ[cur]++;
-                        }
-                        return occ;
-                    }, {});
-                }
-
-                var ssXfun = ['mean', 'sum', 'mode', 'variance', 'standard_deviation', 'standard_deviation', 'median', 'geometric_mean', 'harmonic_mean', 'root_mean_square', 'min', 'max', 'sample_variance'];
-                var c3Nfun = ['bar', 'pie', 'donut'];
-                if (numbers && ssXfun.indexOf(stat) != -1) {
-                    el.text(ss[stat](x));
-                } else if (!numbers && c3Nfun.indexOf(stat) != -1) {
-                    var columns = Object.keys(occurs).reduce(function assoc(memo, key) {
-                            memo.push([key, occurs[key]]);
-                            return memo;
-                        }, []),
-                        oldChart = el.data('chart');
-
-                    if (oldChart) {
-                        var columnsToUnload = Object.keys(oldChart.x())
-                            .diff(columns.reduce(function(memo, key) {
-                                memo.push(key[0]);
-                                return memo;
-                            }, []));
-                        async.series([
-                            function(cba) {
-                                oldChart.load({
-                                    columns: columns,
-                                    // unload: columnsToUnload,
-                                    done: cba,
-                                });
-                            },
-                            function(cba) {
-                                oldChart.unload({
-                                    ids: columnsToUnload,
-                                    done: cba,
-                                });
-                            }
-                        ]);
-                    } else {
-                        var chartSpecs = {
-                            data: {
-                                columns: columns,
-                                type: stat,
-                            },
-                            legend: {
-                                position: 'right'
-                            },
+    // Defining filter functions
+    async.map(filterDef, function findFilterFunction(fl, cbe) {
+        var val = (function findFilterFunctionSync() {
+            switch (fl.type) {
+                case 'simple':
+                case 'regexp':
+                    if (!calls[0][fl.field]) return "No such field for filter " + fl.type;
+                    if (!fl.pattern) return "No pattern field for filter " + fl.field;
+                    if (fl.type == 'simple') {
+                        return function simpleFilter(call, cbf) {
+                            cbf(call[fl.field] == fl.pattern);
                         };
-                        if (['pie', 'donut'].indexOf(stat) != -1) {
-                            chartSpecs[stat] = {
-                                label: {
-                                    format: function(value) {
-                                        return value;
-                                    }
-                                }
-                            };
-                        }
-                        var chart = c3.generate(chartSpecs);
-                        el.empty().append(chart.element).data('chart', chart);
+                    } else { // fl.type == 'regexp'
+                        var regexp = new RegExp(fl.pattern, fl.flags);
+                        return function regexpFilter(call, cbf) {
+                            cbf(call[fl.field].match(regexp));
+                        };
                     }
-                } else {
-                    var statType = (numbers ? '#' : '') + stat;
-                    switch (statType) {
-                        case 'count':
-                        case '#count':
-                            el.text(x.length);
-                            break;
+                    break;
 
-                        case 'undefined':
-                        case '#undefined':
-                        case 'first':
-                        case '#first':
-                            el.text(x[0]);
-                            break;
+                case 'first':
+                    // OPTZ Better handling
+                    return function firstFilter(call, cbf) {
+                        cbf(call == calls[0]);
+                    };
 
-                        case 'last':
-                        case '#last':
-                            el.text(x[x.length - 1]);
-                            break;
+                case 'last':
+                    // OPTZ Better handling
+                    return function lastFilter(call, cbf) {
+                        cbf(call == calls[calls.length - 1]);
+                    };
 
-                        default:
-                            cb("Unknown stat type " + statType);
-                            return;
-                    }
-                }
-            } else {
-                el.removeData();
-                el.text("No data");
+                    // TODO Date ≥/≤ filter
+
+                default:
+                    cb("Unknown filter type " + fl.type);
             }
-            cb();
+        })();
+
+        if (typeof val == 'function') {
+            cbe(null, val);
+        } else {
+            cbe(val);
         }
-    ], function(err) {
+
+    }, function executeFilters(err, filters) {
         if (err) {
-            el.empty().text('???');
             console.warn(err);
+            fil.data('calls', []);
+        } else {
+            async.filter(calls, function applyFilters(call, cbF) {
+                async.every(filters, function applyOneFilter(filter, cbev) {
+                    filter(call, function(res) {
+                        cbev(res);
+                    });
+                }, cbF);
+            }, function onceFiltered(calls) {
+                fil.data('calls', calls);
+                if (cb) cb(null, calls);
+            });
         }
+    });
+}
+
+function updateStat(el, cb) {
+    el = $(el);
+    var parentData = el.parents(':data(calls)').addBack(':data(calls)').last();
+    var calls = parentData.length ? parentData.data('calls') : cdr.data;
+    if (calls && calls.length) {
+        calls = calls.transposeObjects();
+        var field = el.attr('data-field'),
+            stat = el.attr('data-stat'),
+            x = calls[field];
+        if (!x) {
+            cb("No such field " + field);
+        }
+        var numbers = typeof x[0] == 'number'; // Are numbers
+
+        var occurs = null; // Calculates number of occurences
+        if (!numbers) {
+            occurs = x.reduce(function(occ, cur) {
+                if (isNaN(occ[cur])) {
+                    occ[cur] = 1;
+                } else {
+                    occ[cur]++;
+                }
+                return occ;
+            }, {});
+        }
+
+
+        if (numbers && SS_X_FUN.indexOf(stat) != -1) {
+            el.text(ss[stat](x));
+        } else if (!numbers && C3_N_FUN.indexOf(stat) != -1) {
+            var columns = Object.keys(occurs).reduce(function assoc(memo, key) {
+                    memo.push([key, occurs[key]]);
+                    return memo;
+                }, []),
+                oldChart = el.data('chart');
+
+            if (oldChart) {
+                var columnsToUnload = Object.keys(oldChart.x())
+                    .diff(columns.reduce(function(memo, key) {
+                        memo.push(key[0]);
+                        return memo;
+                    }, []));
+                async.series([
+                    function(cba) {
+                        oldChart.load({
+                            columns: columns,
+                            // unload: columnsToUnload,
+                            done: cba,
+                        });
+                    },
+                    function(cba) {
+                        oldChart.unload({
+                            ids: columnsToUnload,
+                            done: cba,
+                        });
+                    }
+                ]);
+            } else {
+                var chartSpecs = {
+                    data: {
+                        columns: columns,
+                        type: stat,
+                    },
+                    legend: {
+                        position: 'right'
+                    },
+                };
+                if (['pie', 'donut'].indexOf(stat) != -1) {
+                    chartSpecs[stat] = {
+                        label: {
+                            format: function(value) {
+                                return value;
+                            }
+                        }
+                    };
+                }
+                var chart = c3.generate(chartSpecs);
+                el.empty().append(chart.element).data('chart', chart);
+            }
+        } else {
+            var statType = (numbers ? '#' : '') + stat;
+            switch (statType) {
+                case 'count':
+                case '#count':
+                    el.text(x.length);
+                    break;
+
+                case 'undefined':
+                case '#undefined':
+                case 'first':
+                case '#first':
+                    el.text(x[0]);
+                    break;
+
+                case 'last':
+                case '#last':
+                    el.text(x[x.length - 1]);
+                    break;
+
+                default:
+                    cb("Unknown stat type " + statType);
+                    return;
+            }
+        }
+    } else {
+        el.removeData();
+        el.text("No data");
+    }
+    cb();
+}
+
+function changed(el, cb) {
+    async.each($(':data(filters)', el).addBack(':data(filters)'), function updateFilters(el, cbe) {
+        updateFilter(el, cbe);
+    }, function() {
+        async.each($('[data-stat],[data-field]'), function updateStats(el, cbe) {
+            updateStat(el, cbe);
+        }, cb);
     });
 }
 
@@ -286,7 +363,7 @@ $(function() {
         function() {
             cdr = new CDR();
             cdr.on('freshData', function() {
-                updateStats();
+                changed(document);
             });
             cdr.update();
             setInterval(function() {
@@ -295,30 +372,46 @@ $(function() {
         }
     ]);
 
-    function updateStats(range) {
-        if (!range) {
-            range = document.body;
-        }
-        $('[data-field]', range).each(function() {
-            // OPTZ Async?
-            fillStat(this);
+    $('[data-filter],[data-filter-field]').each(function extractFiltersFromDOM() {
+        var t = $(this);
+        var f = t.data('filters');
+        if (!f) f = [];
+        var type = t.attr('data-filter');
+        var flags = t.attr('data-filter-flags');
+        f.push({
+            type: type ? type : (flags !== undefined ? 'regexp' : 'simple'),
+            field: t.attr('data-filter-field'),
+            pattern: t.attr('data-filter-pattern'),
+            flags: flags,
         });
-    }
+        t.data('filters', f);
+    });
 
     $('.caller').each(function() {
-        var caller = this;
+        var caller = $(this);
         $('input[name=name]', caller).bind('change keyup paste', function() {
-            var num = $(this).val(),
-                filter = $('[data-filter-field=src]', caller);
-            filter.attr('data-filter-pattern', num ? num : ".*");
-            if (num) {
-                filter.removeAttr('data-filter-flags');
-            } else {
-                filter.attr('data-filter-flags', '');
-            }
-            updateStats(caller);
+            var input = $(this),
+                num = input.val();
+            cdr.callerExists(num, function(exists) {
+                if (num) {
+                    if (exists) {
+                        caller.data('filters', [{
+                            type: 'simple',
+                            field: 'src',
+                            pattern: num,
+                        }]);
+                        changed(caller);
+                        input.css('color', '');
+                    } else {
+                        input.css('color', 'red');
+                    }
+                } else {
+                    caller.data('filters', []);
+                    changed(caller);
+                    input.css('color', '');
+                }
+            });
         });
-
     });
 
     $('.placeholder').css('display', 'none');
